e48b52abb85e9d29aa2ce4fe83341f050a24077c
Dan Snider <dansnider@gmail.com>
2015-04-27 15:54:13 -0400

    about me div

--- javascripts/main.js
+++ javascripts/main.js
@@ -17,8 +17,13 @@ $( window ).scroll( function() {
 	}
 })
 
-$('#about-me-button').click(function() {
-	$('#about-me-reveal').toggle('explode')
-	$('#about-me-text').toggle();
+$('#about-me-button').on('click', function() {
+	$('#about-me-reveal').toggle('slide')
+	if ($(this).hasClass('clicked')) {
+		$('#about-me-button').empty('fade').html("<span id='about-me'>ABOUT ME</span>")
+	} else { 
+		$('#about-me-button').empty('fade').html("<i id='x-out-button' class='mdi-content-clear medium'></i>"); 
+	}
+		$('#about-me-button').toggleClass('clicked')
 })
 

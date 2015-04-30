e51bd8f633870e8234c8d1314108606515184af2
Dan Snider <dansnider@gmail.com>
2015-04-30 17:00:17 -0400

    resume changed

--- javascripts/main.js
+++ javascripts/main.js
@@ -23,9 +23,9 @@ $( window ).scroll( function() {
 $( '#about-me-button' ).on('click', function() {
 	$( '#about-me-reveal' ).toggle('slide')
 	if ( $( this ).hasClass( 'clicked' )) {
-		$( '#about-me-button' ).empty('fade').html("<span id='about-me'>ABOUT ME</span>")
+		$( '#about-me-button' ).empty().html("<span id='about-me'>ABOUT ME</span>")
 	} else { 
-		$( '#about-me-button' ).empty('fade').html("<i id='x-out-button' class='mdi-content-clear medium'></i>"); 
+		$( '#about-me-button' ).empty().html("<i id='x-out-button' class='mdi-content-clear medium'></i>"); 
 	}
 		$( '#about-me-button' ).toggleClass('clicked')
 })
